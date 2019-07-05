2a876bbbd065934c75ab1bf55eb43b58ae1e5872
Fabrice <info@proglab.com>
2019-07-05 18:38:50 +0200

    changement url github

--- client_config.py
+++ client_config.py
@@ -1,7 +1,10 @@
+import application
+
+
 class ClientConfig(object):
     PUBLIC_KEY = 'JDS5MBOstJKItVLgUSBdx56ttBUtzk/jvPVW2Y1lTHc'
     APP_NAME = 'Application'
     COMPANY_NAME = 'Touch Agency'
     HTTP_TIMEOUT = 30
     MAX_DOWNLOAD_RETRIES = 3
-    UPDATE_URLS = ['https://github.com/Proglab/application/releases']
+    UPDATE_URLS = ['https://github.com/Proglab/application/releases/download/v'+application.__version__+'/']
